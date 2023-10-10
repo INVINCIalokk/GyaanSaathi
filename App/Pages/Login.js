82c8017f3e719e1dfad7501a93120f6ae91c9af2
Alok CHOUDHARY <alokc8502@gmail.com>
2023-10-10 17:02:53 +0530

    not_working

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,10 +1,37 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity,} from 'react-native'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet ,onPress} from 'react-native'
 import { AntDesign } from '@expo/vector-icons';
 import Colors from '../Shared/Colors';
+import {
+    GoogleSignin,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
+
 
 export default function Login() {
+    
+    signIn = async () => {
+        GoogleSignin.configure(
+            {androidClientID :"399811484923-5fbnoj9ckei3c7qe13vejkn90m5m72kc.apps.googleusercontent.com"}
+        );
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          setState({ userInfo });
+          
+        } catch (error) {
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            console.log("play services not available or outdated")
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            console.log("play services not available or outdated")
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            console.log("play services not available or outdated")
+          } else {
+            console.log(error)
+          }
+        }
+      };
   return (
     <View>
      <Image source={require('./../Assets/Images/login.png')}/>
@@ -13,10 +40,10 @@ export default function Login() {
         <Text style={{textAlign:'center',
             marginTop:40,fontSize:20}}>Login/Signup</Text>
      </View>
-     <View style={styles.button}>
+     <TouchableOpacity style={styles.button} onPress={signIn}>
      <AntDesign name="google" size={24} color='white' style={{marginRight:20}}/>
         <Text style={{color:Colors.white}}>Sign In with Google</Text>
-     </View>
+     </TouchableOpacity>
     </View>
   )
 }
